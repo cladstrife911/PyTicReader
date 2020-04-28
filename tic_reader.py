e237e7f5ea5ef08ba0b3a9deed74275c2c84deb2
antoine m <cladstrife911@yahoo.fr>
2020-04-28 22:15:37 +0200

    initial commit

---
+++ tic_reader.py
@@ -0,0 +1,65 @@
+# https://techtutorialsx.com/2017/04/23/python-subscribing-to-mqtt-topic/
+
+import getpass 
+import paho.mqtt.client as mqttClient
+import time
+
+Connected = False #global variable for the state of the connection
+ 
+broker_address= "192.168.1.2"
+port = 1883
+user = "emonpi"
+password = ""
+
+####################
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+        global Connected                #Use global variable
+        Connected = True                #Signal connection 
+    else:
+        print("Connection failed")
+
+####################
+def on_message(client, userdata, msg):
+    print("Message received from " + msg.topic + ":" + str(msg.payload))
+
+####################
+# request a password to use for MQTT connection
+def login():
+    _user = getpass.getuser() 
+    if _user != "":
+        user = _user
+    #else use default user name
+    password = getpass.getpass()
+
+####################
+def main():
+    #login()
+
+    client = mqttClient.Client("Python")               #create new instance
+    client.username_pw_set(user, password=password)    #set username and password
+    client.on_connect= on_connect                      #attach function to callback
+    client.on_message= on_message                      #attach function to callback
+
+    client.connect(broker_address, port=port)  #connect to broker
+    client.loop_start()                        #start the loop
+ 
+    while Connected != True:    #Wait for connection
+        time.sleep(0.1)
+ 
+    client.subscribe("AntoineHome/TIC/#")
+
+    try:
+        while True:
+            time.sleep(1)
+ 
+    except KeyboardInterrupt:
+        print("exiting")
+        client.disconnect()
+        client.loop_stop()
+
+
+####################
+if __name__ == "__main__":
+    main()
